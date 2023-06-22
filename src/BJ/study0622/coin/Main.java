c658cf842f8e3a8820c96222a939f6551765b3e1
sunsik <sunsig21c@gmail.com>
2023-06-22 23:24:38 +0900

    dynamic programing algorithm

---
+++ src/BJ/study0622/coin/Main.java
@@ -0,0 +1,41 @@
+package BJ.study0622.coin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, k;
+	static int[] dp;
+	static List<Integer> coins = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		dp = new int[k + 1];
+		dp[0] = 1;
+
+		while(n-- > 0) {
+			coins.add(Integer.parseInt(br.readLine()));
+		}
+		countCoinChange();
+
+		System.out.println(dp[k]);
+	}
+	private static void countCoinChange() {
+		for (int coin : coins) {
+			for (int i = coin; i < k + 1; i++) {
+				dp[i] += dp[i - coin];
+			}
+		}
+	}
+
+}
