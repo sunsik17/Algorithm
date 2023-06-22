e305a6337bd682292511f7da2fea69a3e5d3d581
sunsik <sunsig21c@gmail.com>
2023-06-22 23:49:12 +0900

    정렬하여 겹치는 부분 체크

---
+++ src/프로그래머스/study0622/요격시스템/Solution.java
@@ -0,0 +1,40 @@
+package 프로그래머스.study0622.요격시스템;
+
+import java.util.Arrays;
+class Solution {
+	public int solution(int[][] targets) {
+		int answer = 0;
+
+		Arrays.sort(targets, (x,y) -> x[1] - y[1]);
+
+		int right = -1;
+
+		for(int[] arr : targets) {
+			if (right == -1) {
+				answer++;
+				right = arr[1] - 1;
+				continue;
+			}
+			if (right >= arr[0] && right <= arr[1]) continue;
+
+			answer++;
+			right = arr[1] - 1;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+
+		int[][] targets = {
+			{4, 5},
+			{4, 8},
+			{10, 14},
+			{11, 13},
+			{5, 12},
+			{3, 7},
+			{1, 4},
+		};
+		System.out.println(new Solution().solution(targets));
+	}
+}
