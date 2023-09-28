90d23961b28ad20e399f1802d839fe826ea86c74
sunsik17 <117346927+sunsik17@users.noreply.github.com>
2023-09-28 21:42:46 +0900

    백준 용액 합성하기 문제풀이 코드
    백준 용액 합성하기 문제풀이 코드
    이분탐색

---
+++ src/백준/d0928용액합성하기/용액합성하기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+
+    static int N;
+    static int[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+        int right = N - 1;
+
+        int answer = Integer.MAX_VALUE;
+        while (left < right) {
+            int result = A[left] + A[right];
+            if (result > 0) {
+                right--;
+            } else if (result < 0) {
+                left++;
+            } else {
+                System.out.println(result);
+                return;
+            }
+            if (Math.abs(result) < Math.abs(answer)) {
+                answer = result;
+            }
+        }
+        System.out.println(answer);
+    }
+}
