e000f04373f505cfe0a7698459522fc6d01e3cca
sunsik17 <117346927+sunsik17@users.noreply.github.com>
2023-10-19 22:28:44 +0900

    백준 서로소 평균 문제 풀이
    백준 서로소 평균 문제 풀이
    
    서로소 = 두 수 사이의 최대 공약수가 1 이거나 최소공배수가 두수를 곲한 값일 때
    
    gcd() 유클리드호제법 사용

---
+++ src/백준/1019서로소평균/서로소평균.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+class 서로소평균 {
+    static int n, x;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        x = Integer.parseInt(br.readLine());
+
+        double sum = 0;
+        double count = 0;
+        for (int i : arr) {
+            if (gcd(i, x) == 1) {
+                sum += i;
+                count++;
+            }
+        }
+        System.out.print(sum / count);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
