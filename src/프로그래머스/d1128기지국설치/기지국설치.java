be3c291df645f3ce790d0bedf3a5b28bfc2f2369
sunsik17 <117346927+sunsik17@users.noreply.github.com>
2023-11-28 22:20:49 +0900

    프로그래머스 기지국 설치 문제풀이
    프로그래머스 기지국 설치 문제풀이
    
    N이 2억, stations의 길이가 최대 1만
    O(n) 시간복잡도로 풀이해야 한다고 판단했고
    이후 stations의 원소를 이용해 반복문 1회로 문제풀이

---
+++ src/프로그래머스/d1128기지국설치/기지국설치.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int idx = 1;
+        int len = (w * 2) + 1;
+        for (int i = 0; i < stations.length; i++) {
+            int tmp = stations[i] - w;
+            if (idx < tmp) {
+                if ((tmp - idx) % len == 0) {
+                    answer += (tmp - idx) / len;
+                } else {
+                    answer += ((tmp - idx) / len) + 1;
+                }
+            }
+            idx = stations[i] + w + 1;
+        }
+
+        if (idx <= n) {
+            int dif = n - idx + 1;
+            if (dif % len == 0) {
+                answer += dif / len;
+            } else {
+                answer += dif / len + 1;
+            }
+        }
+        return answer;
+    }
+}
