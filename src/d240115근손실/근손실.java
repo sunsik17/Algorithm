d9d996874123dce1c5cc1fabf656cd23fefb1a00
sunsik17 <sunsig21c@gmail.com>
2024-01-15 18:00:51 +0900

    백준 문제 근손실
    전체 탐색 + 백트래킹 알고리즘 for java

---
+++ src/d240115근손실/근손실.java
@@ -0,0 +1,54 @@
+package d240115근손실;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 근손실 {
+	static int N, K;
+	static boolean[] visited;
+	static int[] kits;
+	static int answer, count = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[N];
+		kits = new int[N];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			kits[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dfs(500);
+		System.out.println(answer);
+	}
+
+	private static void dfs(int w) {
+		if (w < 500) {
+			return;
+		}
+
+		if (count == N) {
+			answer++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				count++;
+				w = w - K + kits[i];
+				dfs(w);
+				w = w + K - kits[i];
+				count--;
+				visited[i] = false;
+			}
+		}
+	}
+
+}
